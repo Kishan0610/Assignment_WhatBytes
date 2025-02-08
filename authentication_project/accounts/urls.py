c490b2b8518cc6050ee4ca593a662890174e5554
Ravikishan Gupta <061002kishan@gmail.com>
2025-02-08 18:19:54 +0530

    Initial commit for authentication project

---
+++ authentication_project/accounts/urls.py
@@ -0,0 +1,20 @@
+from django.urls import path
+from django.contrib.auth import views as auth_views
+from .views import signup_view, login_view, dashboard_view, profile_view, logout_view
+
+urlpatterns = [
+    path('signup/', signup_view, name='signup'),
+    path('login/', login_view, name='login'),
+    path('logout/', logout_view, name='logout'),
+
+    path('dashboard/', dashboard_view, name='dashboard'),
+    path('profile/', profile_view, name='profile'),
+
+    path('password_change/', auth_views.PasswordChangeView.as_view(template_name='accounts/password_change.html'), name='password_change'),
+    path('password_change/done/', auth_views.PasswordChangeDoneView.as_view(template_name='accounts/password_change_done.html'), name='password_change_done'),
+
+    path('password_reset/', auth_views.PasswordResetView.as_view(template_name="accounts/password_reset.html"), name='password_reset'),
+    path('password_reset_done/', auth_views.PasswordResetDoneView.as_view(template_name="accounts/password_reset_done.html"), name='password_reset_done'),
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name="accounts/password_reset_confirm.html"), name='password_reset_confirm'),
+    path('reset/done/', auth_views.PasswordResetCompleteView.as_view(template_name="accounts/password_reset_complete.html"), name='password_reset_complete'),
+]
---
+++ authentication_project/accounts/views.py
@@ -0,0 +1,47 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, logout
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.forms import AuthenticationForm
+from .forms import SignupForm
+
+# Create your views here.
+
+def home(request):
+    return render(request, 'accounts/home.html')
+
+def signup_view(request):
+    if request.method == 'POST':
+        form = SignupForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)  # Log in after signup
+            return redirect('login')
+    else:
+        form = SignupForm()  
+
+    return render(request, 'accounts/signup.html', {'form': form})  # Pass form even if invalid
+
+
+def login_view(request):
+    form = AuthenticationForm(request, data=request.POST or None)
+    
+    if request.method == 'POST':
+        if form.is_valid():  # Checks username & password
+            user = form.get_user()
+            login(request, user)
+            return redirect('dashboard')
+
+    return render(request, 'accounts/login.html', {'form': form})
+
+@login_required
+def dashboard_view(request):
+    return render(request, 'accounts/dashboard.html', {'user': request.user})
+
+@login_required
+def profile_view(request):
+    return render(request, 'accounts/profile.html', {'user': request.user})
+
+@login_required
+def logout_view(request):
+    logout(request)
+    return redirect('login')
